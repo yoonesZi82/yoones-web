1861883fad42142691cdefb38d2dd573cfcd8fcd
yoones <yooneszaamni58@gmail.com>
2024-08-12 22:15:39 +0330

    fix() : set new config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  important: true,
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,11 +9,25 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        normalBlack: "#111111",
+        meloWhite: "#faf4f0",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+    },
+    screens: {
+      mobile: "640px",
+      tablet: "768px",
+      laptop: "1024px",
+      xl: "1200px",
+      desktop: "1280px",
+    },
+    container: {
+      center: true,
     },
   },
   plugins: [require("daisyui")],
