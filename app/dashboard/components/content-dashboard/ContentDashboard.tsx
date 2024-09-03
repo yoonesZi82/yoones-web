c57b308447d9c2409b936f7652880bd4aec7fba0
yoones <yooneszaamni58@gmail.com>
2024-09-03 11:24:37 +0330

    feat() : create dashboard page

--- app/dashboard/components/content-dashboard/ContentDashboard.tsx
+++ app/dashboard/components/content-dashboard/ContentDashboard.tsx
@@ -1,12 +1,21 @@
+"use client";
 import React from "react";
 import { Layout } from "antd";
+import { usePathname } from "next/navigation";
 
 const { Content } = Layout;
 
 function ContentDashboard({ children }: { children: React.ReactNode }) {
+  const path = usePathname();
   return (
-    <Content className="desktop:my-[18px] laptop:my-[20px] my-[50px] mobile:my-[50px] tablet:my-[45px] mr-[20px]">
-      <div className="bg-[#141E30] p-6 rounded-xl min-h-[360px]">
+    <Content className="desktop:pt-[18px] laptop:pt-[20px] bg-black px-10 pt-[50px] mobile:pt-[50px] tablet:pt-[45px]">
+      <div
+        className={
+          path === "/dashboard"
+            ? "p-6 min-h-[360px] bg-[rgba(255,255,255,0.1)] rounded-2xl "
+            : "bg-[rgba(255,255,255,0.1)] p-2 rounded-2xl min-h-[360px] max-h-[645px] overflow-y-auto"
+        }
+      >
         {children}
       </div>
     </Content>
