fe22126c9334343320f8e8ca2ff8a203b4abc6b1
yoones <yooneszaamni58@gmail.com>
2024-09-03 11:17:48 +0330

    feat() : create dashboard components

---
+++ app/dashboard/blogs/new/components/tag-input/TagInput.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Input, Alert } from "antd";
+import { Controller } from "react-hook-form";
+import InputPropsType from "../types/inputType";
+import { PiArticleNyTimesFill } from "react-icons/pi";
+
+function TagInput({ control, error, defaultValue }: InputPropsType) {
+  return (
+    <div>
+      <Controller
+        name="tag"
+        control={control}
+        defaultValue={defaultValue}
+        render={({ field }) => (
+          <Input
+            size="large"
+            addonBefore={<PiArticleNyTimesFill size={20} color="#faf4f0" />}
+            placeholder="Tag *"
+            {...field}
+          />
+        )}
+      />
+      {error && (
+        <Alert
+          message={error}
+          type="warning"
+          showIcon
+          className="mt-2 h-[32px] text-[12px]"
+        />
+      )}
+    </div>
+  );
+}
+
+export default TagInput;
