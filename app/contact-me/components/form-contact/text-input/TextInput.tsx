666fbeca4ca967669f7507c1d65929ea2c5a8640
yoones <yooneszaamni58@gmail.com>
2024-09-03 11:11:55 +0330

    feat() : create contact form

--- app/contact-me/components/form-contact/text-input/TextInput.tsx
+++ app/contact-me/components/form-contact/text-input/TextInput.tsx
@@ -14,6 +14,7 @@ function TextInput({ control, error }: InputPropsType) {
           <TextArea
             autoSize={{ minRows: 3, maxRows: 4 }}
             placeholder="Text *"
+            className="pt-2 font-medium"
             {...field}
           />
         )}
