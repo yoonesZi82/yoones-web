68bb8d560822e7bd9aec6ec06962bea60c3a5e25
yoones <yooneszaamni58@gmail.com>
2024-09-06 11:41:41 +0330

    feat() : create login button

--- app/login/components/login-form/button-input/LoginButton.tsx
+++ app/login/components/login-form/button-input/LoginButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { Button } from "antd";
-import { PiPaperPlaneTilt } from "react-icons/pi";
+import { PiSignInBold } from "react-icons/pi";
 import FormBtnPropsTypes from "../types/buttonTypes";
 
 const ButtonForm = ({ loading }: FormBtnPropsTypes) => {
@@ -10,9 +10,9 @@ const ButtonForm = ({ loading }: FormBtnPropsTypes) => {
       <Button
         htmlType="submit"
         size="large"
-        icon={<PiPaperPlaneTilt size={20} />}
+        icon={<PiSignInBold size={20} />}
         loading={loading}
-        className="flex flex-row-reverse justify-center items-center bg-meloWhite hover:bg-gray-200 py-4 border-none w-full text-3xl text-color-white text-normalBlack hover:text-normalBlack transition-all duration-500 send_btn_form"
+        className="flex flex-row-reverse justify-center items-center bg-meloWhite hover:bg-gray-200 shadow-[10px_10px_10px_#babecc,-10px_-10px_20px_#ffffff] py-4 border-none w-full text-3xl text-color-white text-normalBlack hover:text-normalBlack transition-all duration-500 send_btn_form"
       >
         Login
       </Button>
